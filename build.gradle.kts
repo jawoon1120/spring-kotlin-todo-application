2f97a912ea2a09eaebb29bb4d8f3aef399389804
jeong <jw.jeong@tilda.co.kr>
2025-10-12 22:18:48 +0900

    config: flyway 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,9 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.postgresql:postgresql:42.7.3")
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-database-postgresql")
+
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
