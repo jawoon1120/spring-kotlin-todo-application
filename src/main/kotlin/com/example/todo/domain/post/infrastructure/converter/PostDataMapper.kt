a09b66fad547d88ec30ccc878cd71362a75c9342
jeong <jw.jeong@tilda.co.kr>
2025-10-21 22:34:21 +0900

    bugfix: save new entity with id

--- src/main/kotlin/com/example/todo/domain/post/infrastructure/converter/PostDataMapper.kt
+++ src/main/kotlin/com/example/todo/domain/post/infrastructure/converter/PostDataMapper.kt
@@ -9,19 +9,22 @@ import java.time.ZoneId
 @Component
 class PostDataMapper {
     fun toAggregate(postEntity: PostEntity): PostRootAggregate =
-        PostRootAggregate(
-            id = postEntity.id!!,
+        PostRootAggregate.restore(
+            id = postEntity.id,
             title = postEntity.title,
             content = postEntity.content,
-            createdAt = postEntity.createdAt!!.atZone(ZoneId.systemDefault()).toInstant()
+            createdAt = postEntity.createdAt.atZone(ZoneId.systemDefault()).toInstant()
         )
 
-    fun toEntity(postAggregate: PostRootAggregate): PostEntity =
-        PostEntity(
+    fun toEntity(postAggregate: PostRootAggregate, isNew: Boolean): PostEntity {
+        val postEntity = PostEntity(
             id = postAggregate.id,
             title = postAggregate.title,
             content = postAggregate.content,
             createdAt = postAggregate.createdAt.atZone(ZoneId.systemDefault()).toLocalDateTime()
         )
+        if (!isNew) { postEntity.markNotNew()}
 
+        return postEntity
+    }
 }
--- src/main/kotlin/com/example/todo/domain/post/infrastructure/entity/PostEntity.kt
+++ src/main/kotlin/com/example/todo/domain/post/infrastructure/entity/PostEntity.kt
@@ -3,6 +3,7 @@ package com.example.todo.domain.post.infrastructure.entity
 import jakarta.persistence.*
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.domain.Persistable
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
 import java.util.*
@@ -10,27 +11,36 @@ import java.util.*
 @Entity
 @Table(name = "post")
 @EntityListeners(AuditingEntityListener::class)
-class PostEntity (
+class PostEntity  (
     @Id
-    @GeneratedValue
-    @Column(columnDefinition = "UUID", updatable = false, nullable = false)
-    val id: UUID? = null,
+    @Column(columnDefinition = "UUID")
+    private val id: UUID,
 
-    @Column
+    @Column(nullable = false)
     var title: String,
 
-    @Column
+    @Column(nullable = false)
     var content: String,
 
     @CreatedDate
     @Column(updatable = false, nullable = false)
-    var createdAt: LocalDateTime,
+    var createdAt: LocalDateTime = LocalDateTime.now(),
 
     @LastModifiedDate
-    @Column(nullable = false)
+    @Column
     var updatedAt: LocalDateTime? = null,
 
     @Column
-    var deletedAt: LocalDateTime? = null
-)
+    var deletedAt: LocalDateTime? = null,
 
+    @Transient
+    private var _isNew: Boolean = true
+): Persistable<UUID>{
+    override fun getId(): UUID = id
+
+    override fun isNew(): Boolean = _isNew
+
+    fun markNotNew() {
+        _isNew = false
+    }
+}
